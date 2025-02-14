dd5aa67ac0a1a0b7a0058e61eb64e68d047eb459
Ye Marn Aung <yemarnaung441@gmail.com>
2025-02-14 17:56:05 +0700

    Create App Bar

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      
+      <h1>Hello Next JS</h1>
+    </div>
+  );
+}
